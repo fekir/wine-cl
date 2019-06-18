f73930355953c57051b619587e128feef0244627
Federico Kircheis <federico.kircheis@gmail.com>
2019-06-18 17:26:59 +0200

    Cleanup tests
    And improve x86 support

--- test/lib/winmain.cpp
+++ test/lib/winmain.cpp
@@ -2,10 +2,9 @@
 
 #include "lib.hpp"
 
-//#include <winbase.h>
 #include <Windows.h>
 
-int WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	std::cout << "Hello World (C++, WinMain, lib)!\n";
 	return testlib(0);
 }
--- test/winmain.cpp
+++ test/winmain.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 
-//#include <winbase.h>
 #include <Windows.h>
 
-int WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	std::cout << "Hello World (C++, WinMain)!\n";
 	return 0;
 }
